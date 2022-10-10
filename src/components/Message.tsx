4978ffe7dbd887bb30bd0ca8e110d93fbdbca060
pedrorfdez <pfrascon24@gmail.com>
2022-10-10 21:38:22 +0200

    UI Design finished

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className='messageInfo'>
         <img
           src='https://images.pexels.com/photos/236588/pexels-photo-236588.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
@@ -12,10 +12,10 @@ const Message = () => {
       </div>
       <div className='messageContent'>
         <p>hello</p>
-        {/*<img
+        <img
           src='https://images.pexels.com/photos/236588/pexels-photo-236588.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
           alt=''
-        />*/}
+        />
       </div>
     </div>
   );
